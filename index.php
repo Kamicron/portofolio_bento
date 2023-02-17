ab76dc0a9a0dcc21fa43cd249416b539ac27f597
Kamicron <ludovic.chevroulet@outlook.fr>
2023-02-17 14:30:45 +0100

    Display contact, et début cv

--- index.php
+++ index.php
@@ -6,24 +6,66 @@
 
 <section class="section section--left">
   <div class="information">
-    <h1>Ludovic CHEVROULET</h1>
+    <h1 class="title_bg-black">Ludovic CHEVROULET</h1>
     <div class="profile-icon">
       <img src="asset/photo/ludovic_chevroulet.jpg" alt="Votre nom">
     </div>
-    <div> 
-    Initialement technicien en CEM, j’ai choisis de m’orienter sur le développement web, un métier que j’ai découvert lors de ma dernière mission, afin de me positionner sur le marché du travailr
-    </div>
+    <div>
+      Je suis un développeur junior axé sur le back-end, spécialisé dans la création de sites web efficaces et performants, avec une attention particulière à l'expérience utilisateur. </div>
   </div>
-  <div class="contact">
-    <h2>Contactez moi</h2>
+  <div class="contact-form">
+    <h2  class="title_bg-black">Contactez-moi</h2>
+    <form>
+      <div>
+        <label for="name">Nom:</label>
+        <input type="text" id="name" name="name" required>
+      </div>
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+      </div>
+      <div>
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+      </div>
+      <button type="submit">Envoyer</button>
+    </form>
   </div>
 </section>
 <section class="section section--right">
   <div class="cv">
-    <h2>Mon CV</h2>
+  <h2  class="title_bg-white">Mon CV</h2>
+
+  <div class="cv__section">
+    <h3 class="cv__section-title">Formation</h3>
+    <div class="cv__section-item">
+      <h4 class="cv__section-item-title">Titre de la formation</h4>
+      <p class="cv__section-item-date">Date de la formation</p>
+      <p class="cv__section-item-description">Description de la formation</p>
+      <ul class="cv__section-item-tags">
+        <li class="cv__section-item-tag">HTML</li>
+        <li class="cv__section-item-tag">CSS</li>
+        <li class="cv__section-item-tag">JavaScript</li>
+      </ul>
+    </div>
   </div>
+  <div class="cv__section">
+    <h3 class="cv__section-title">Expérience professionnelle</h3>
+    <div class="cv__section-item">
+      <h4 class="cv__section-item-title">Titre du poste</h4>
+      <p class="cv__section-item-date">Date de l'expérience professionnelle</p>
+      <p class="cv__section-item-description">Description de l'expérience professionnelle</p>
+      <ul class="cv__section-item-tags">
+        <li class="cv__section-item-tag">PHP</li>
+        <li class="cv__section-item-tag">SQL</li>
+        <li class="cv__section-item-tag">Java</li>
+      </ul>
+    </div>
+  </div>
+</div>
+
   <div class="project">
-    <h2>Mes projets</h2>
+    <h2  class="title_bg-white">Mes projets</h2>
     <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum tempore quod facilis corrupti, voluptates aliquam adipisci enim repellat quia aliquid accusamus expedita accusantium modi cumque corporis voluptas perspiciatis voluptatibus quisquam.</div>
     <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum tempore quod facilis corrupti, voluptates aliquam adipisci enim repellat quia aliquid accusamus expedita accusantium modi cumque corporis voluptas perspiciatis voluptatibus quisquam.</div>
     <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum tempore quod facilis corrupti, voluptates aliquam adipisci enim repellat quia aliquid accusamus expedita accusantium modi cumque corporis voluptas perspiciatis voluptatibus quisquam.</div>
